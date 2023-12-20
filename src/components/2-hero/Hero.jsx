c0248465423ea73fd8e9ea1f706a6e6b8c0a9e68
mazensabry2712 <111mazensabry111@gmail.com>
2023-12-21 01:39:49 +0200

    reactportfolio

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,66 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="/public/image.jpg"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Software Engineer, Mern stack Developer, Designer.{" "}
+        </motion.h1>
+
+        <p className="sub-title">
+          I am Eng/ Mazen Sabry, an Egyptian software engineer, specializing in
+          web engineering, especially Mernstack, residing in Cairo. I work as a
+          freelancer, where I develop technologies that enable ordinary people
+          to use them, secure them, and do everything related to the programming
+          field.
+        </p>
+
+        <div className="all-icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
